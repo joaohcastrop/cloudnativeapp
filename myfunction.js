339b3abdda611525337630d0bf1bb6c8ffb4fedd
João Castro <joaohcastrop@gmail.com>
2025-02-27 17:52:38 -0300

    finalizando

--- myfunction.js
+++ myfunction.js
@@ -1,6 +1,6 @@
 function myFunction(a, b) {
-    var c = JSON.parse(a);
-    var d = c + JSON.parse(b);
-    return d;
-    }
-    export default myFunction;
+  var c = JSON.parse(a);
+  var d = c + JSON.parse(b);
+  return d;
+  }
+  export default myFunction;
